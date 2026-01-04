1353828a6399c2bd61468fa0d660c9f4245d3111
cocktailpeanut <121128867+cocktailpeanut@users.noreply.github.com>
2026-01-04 18:06:12 -0500

    Update pinokio.js

--- pinokio.js
+++ pinokio.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: "Zed",
   link: "https://zed.dev/",
-  icon: "icon.png",
+  icon: "icon.webp",
   description: "Zed is a next-generation code editor designed for high-performance collaboration with humans and AI.",
    path: "plugin",
   update: [{
